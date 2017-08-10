333393a64169cfef10388d1fb2e68e5b5dc3547d
'mldn' <'2582129519@qq.com'>
2017-08-10 23:16:23 +0800

    增加了PreparedStatement2.java文件

---
+++ src/cn/susu/preparedstatement/PreparedStatementDemo2.java
@@ -0,0 +1,27 @@
+package cn.susu.preparedstatement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Date;
+
+public class PreparedStatementDemo2 {
+	private static final String DBDRIVER = "oracle.jdbc.driver.OracleDriver";
+	private static final String DBURL = "jdbc:oracle:thin:@localhost:1521:tsu";
+	private static final String USER = "scott";
+	private static final String PASSWORD = "tiger";
+	public static void main(String[] args) throws Exception {
+		String name = "Mr'SMITH"; 
+		Date birthday = new Date();
+		int age = 18;
+		String note = "ÊÇ¸öÍá¹ûÈÊ";
+		
+		Class.forName(DBDRIVER);
+		
+		Connection conn = DriverManager.getConnection(DBURL);
+		String sql = " INSERT INTO member(mid,name,birthday,age,note) VALUES "
+				   + " (myseq.nextval,?,?,?,?) ";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+	}
+
+}
